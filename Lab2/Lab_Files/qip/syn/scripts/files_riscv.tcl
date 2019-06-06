3bcc38de9348b0cb8b0071d61bf01d57f25ecc69
Jiajun Chang <jiajunc@uci.edu>
2019-06-06 12:14:34 -0700

    add syn

---
+++ Lab2/Lab_Files/qip/syn/scripts/files_riscv.tcl
@@ -0,0 +1,16 @@
+analyze -format sverilog { \
+  adder.sv \
+  alu.sv \
+  alu_controller.sv \
+  controller_if.sv\
+  controlpath.sv\
+  data_memory.sv\
+  datapath.sv\
+  immediate_generator.sv\
+  instruction_memory.sv \
+  memory_if.sv \
+  mux2.sv \
+  program_counter.sv \
+  riscv_package.sv \
+  riscv.sv \
+}
